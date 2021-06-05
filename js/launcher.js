f1bef545caa0ab2cfa009c18c67dc3770719428f
vanya12khanna <76106237+vanya12khanna@users.noreply.github.com>
2021-06-05 18:17:30 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,27 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 15
+        }
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+        this.pointB= pointB;
+    }
+
+    display(){
+        
+        if(this.launcher.bodyA){
+            var pointA = this.launcher.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+   fly(){
+       this.launcher.bodyA = null
+   }
+}
